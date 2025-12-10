41bedb18afeea81c33d177805c1ab74b6c0f73ca
Abinav <abinav.g2022ece@sece.ac.in>
2025-12-10 10:31:09 +0530

    pg8

---
+++ regularExpression2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.regex.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inp = sc.nextLine();
+        System.out.println(UserMainCode.regularExpression(inp));
+    }
+}
+
+class UserMainCode {
+    public static boolean regularExpression(String inp) {
+        /*Pattern p = Pattern.compile("[a-zA-Z0-9] R[0-9] [a-zA-Z0-9]");
+        Matcher m = p.matcher(inp);
+        return m.matches();*/
+        return inp.matches("(^[2][1-9]$)|(^[3][0-9]$)|(^[4][0-5]$)");
+    }
+}
